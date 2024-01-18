fe22e2dc4c2add12a143e2f369fc642e2da33742
fangxian <Fang_xian0506@163.com>
2024-01-18 12:21:12 +0800

    Initial commit userName

--- src/Socket/AdapterSocket/SOCKS5AuthAdapter.swift
+++ src/Socket/AdapterSocket/SOCKS5AuthAdapter.swift
@@ -1,17 +1,18 @@
 import Foundation
 
+/**socks5的账号密码*/
 public class socks5Auth: NSObject {
     public var host: String!
     public var port: Int!
     public var username: String!
     public var password: String!
-    public var endtime: TimeInterval!
+    public var endtime:  TimeInterval!
 }
 
 public class SOCKS5AuthAdapterFactory: ServerAdapterFactory {
     let username: String
     let password: String
-    let endtime: TimeInterval
+    let endtime:  TimeInterval
     
     public init(_ auth: socks5Auth) {
         self.username = auth.username
@@ -30,83 +31,64 @@ public class SOCKS5AuthAdapterFactory: ServerAdapterFactory {
 class SOCKS5AuthAdapter: SOCKS5Adapter {
     let username: Data
     let password: Data
-    let endtime: TimeInterval
+    let endtime:  TimeInterval
     var waitAuthResult: Bool = false
-    
-    init(serverHost: String, serverPort: Int, username: String, password: String, endtime: TimeInterval) {
-        self.username = convertToData(username)
-        self.password = convertToData(password)
+
+    public init(serverHost: String, serverPort: Int, username: String, password: String, endtime: TimeInterval) {
+        self.username = username.data(using: .utf8)!
+        self.password = password.data(using: .utf8)!
         self.endtime  = endtime
         super.init(serverHost: serverHost, serverPort: serverPort)
         self.helloData = Data([0x05, 0x01, 0x02])
     }
-    
+
     override func didConnectWith(socket: RawTCPSocketProtocol) {
         super.didConnectWith(socket: socket)
-        internalStatus = .connecting
+
+        internalStatus = .connecting // 让它继续处于 .connection 的状态，这样可以在 super.didRead 时跳过数据处理
     }
 
     override func didRead(data: Data, from socket: RawTCPSocketProtocol) {
         super.didRead(data: data, from: socket)
-        
-        if endtime < Date().timeIntervalSince1970 {
+        // 判断是否过期，过期就断开连接
+        if endtime<Date().timeIntervalSince1970 {
             disconnect()
-            // 考虑优雅地处理过期错误
-            return
+            exit(0)
         }
-        
         if internalStatus == .connecting {
-            handleConnectingData(data)
-        }
-    }
-    
-    private func handleConnectingData(_ data: Data) {
-        guard data.count == 2 else {
-            disconnect()
+            if data.count != 2 { // 无论是协商认证，还是认证结果，返回字节都是 2
+                disconnect()
+                return
+            }
+            if waitAuthResult == false { // 连接成功，等待发送密码回去
+                let handshake = [UInt8](data)
+                if handshake[0] == 0x05 && handshake[1] == 0x02 {
+                    var auth: [UInt8] = [0x01, UInt8(username.count)]
+                    auth += [UInt8](username)
+                    auth += [UInt8(password.count)]
+                    auth += [UInt8](password)
+
+                    write(data: Data(auth))
+
+                    waitAuthResult = true
+                    socket.readDataTo(length: 2)
+                } else { // 协议失败,退出VPN
+                    disconnect()
+                    return
+                }
+            } else { // 返回了认证结果
+                let authresult = [UInt8](data)
+                if authresult[0] == 0x01 && authresult[1] == 0x00 {
+                    // 认证成功，进入 SOCKS5Adapter.didRead 处理流程
+                    internalStatus = .readingMethodResponse
+                    self.didRead(data: Data(), from: socket)
+                } else {
+                    // 退出之前先进行保存退出原因，登录APP后就可以得知
+                    disconnect()
+                    exit(0)
+                }
+            }
             return
         }
-        
-        if waitAuthResult == false {
-            handleConnectionSuccess()
-        } else {
-            handleAuthResult(data)
-        }
-    }
-    
-    private func handleConnectionSuccess() {
-        let handshake = [UInt8](data)
-        if handshake[0] == 0x05 && handshake[1] == 0x02 {
-            sendAuthData()
-            waitAuthResult = true
-            socket.readDataTo(length: 2)
-        } else {
-            disconnect()
-            // 优雅地处理协议失败
-        }
-    }
-    
-    private func handleAuthResult(_ data: Data) {
-        let authResult = [UInt8](data)
-        if authResult[0] == 0x01 && authResult[1] == 0x00 {
-            internalStatus = .readingMethodResponse
-            didRead(data: Data(), from: socket)
-        } else {
-            disconnect()
-            // 优雅地处理身份验证失败
-        }
-    }
-    
-    private func sendAuthData() {
-        write(data: username)
-        write(data: password)
-    }
-    
-    private func convertToData(_ input: String) -> Data {
-        guard let numericValue = Int(input) else {
-            return Data(input.utf8)
-        }
-        
-        var intValue = numericValue
-        return Data(bytes: &intValue, count: MemoryLayout.size(ofValue: intValue))
     }
 }
